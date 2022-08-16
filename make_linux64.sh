be482538edaee0d5ff116d633b7e0500b51bde87
johnche <johnche@tencent.com>
2022-08-16 10:15:42 +0800

    文件拷贝错误

--- make_linux64.sh
+++ make_linux64.sh
@@ -3,4 +3,4 @@ cmake ../
 cd ..
 cmake --build build_linux64 --config Release
 mkdir -p ./qjs/quickjs/Lib/Linux/
-cp build_linux64/Release/libquickjs.a ./qjs/quickjs/Lib/Linux/
+cp build_linux64/libquickjs.a ./qjs/quickjs/Lib/Linux/
