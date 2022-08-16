979fac1f5158070f3d20b8ff5e839e41ccc65176
johnche <johnche@tencent.com>
2022-08-16 10:12:20 +0800

    添加Linux编译支持

---
+++ make_linux64.sh
@@ -0,0 +1,6 @@
+mkdir -p build_linux64 && cd build_linux64
+cmake ../
+cd ..
+cmake --build build_linux64 --config Release
+mkdir -p ./qjs/quickjs/Lib/Linux/
+cp build_linux64/Release/libquickjs.a ./qjs/quickjs/Lib/Linux/
