2982066913a2abdfb4928871689c8673a901390d
johnche <johnche@tencent.com>
2024-04-11 11:54:57 +0800

    修改linux编译参数和puerts plugin一致，解决linux下的崩溃

--- make_linux64.sh
+++ make_linux64.sh
@@ -5,7 +5,7 @@ else
 fi
 
 mkdir -p build_linux64 && cd build_linux64
-cmake -DQJS_NS=${QJSNS} ../
+cmake -DQJS_NS=${QJSNS} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ../
 cd ..
 cmake --build build_linux64 --config Release
 mkdir -p ./qjs/quickjs/Lib/Linux/
