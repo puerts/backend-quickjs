2f776e86c4fb59b339cf003cde9864fbe5774638
johnche <johnche@tencent.com>
2024-04-19 14:01:41 +0800

    鸿蒙编译支持

---
+++ make_ohos.sh
@@ -0,0 +1,25 @@
+if [ -n "$1" ]; then
+    export QJSNS=1
+else
+    export QJSNS=0
+fi
+
+if [ -n "$OHOS_NDK" ]; then
+    export NDK=${OHOS_NDK}
+else
+    export NDK=/mnt/d/ohos-sdk/
+fi
+
+export PATH=${NDK}/linux/native/build-tools/cmake/bin:$PATH
+
+function build() {
+    ABI=$1
+    BUILD_PATH=build.OHOS.${ABI}
+    cmake -H. -DOHOS_STL=c++_shared -B${BUILD_PATH} -DOHOS_ARCH=${ABI} -DOHOS_PLATFORM=OHOS -DCMAKE_TOOLCHAIN_FILE=${NDK}/linux/native/build/cmake/ohos.toolchain.cmake -DQJS_NS=${QJSNS} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+    cmake --build ${BUILD_PATH} --config Release
+    #mkdir -p ./qjs/quickjs/Lib/OHOS/${ABI}/
+    #cp ${BUILD_PATH}/libquickjs.a ./qjs/quickjs/Lib/OHOS/${ABI}/libquickjs.a
+}
+
+build armeabi-v7a
+build arm64-v8a
