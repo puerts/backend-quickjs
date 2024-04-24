d47e699730d01332e5267e04eb9e5638b8318c4f
johnche <johnche@tencent.com>
2024-04-24 14:49:59 +0800

    release加上鸿蒙版本

--- make_ohos.sh
+++ make_ohos.sh
@@ -6,19 +6,21 @@ fi
 
 if [ -n "$OHOS_NDK" ]; then
     export NDK=${OHOS_NDK}
+elif [ -n "$OHOS_NDK_HOME" ]; then
+    export NDK=${OHOS_NDK_HOME}
 else
-    export NDK=/mnt/d/ohos-sdk/
+    export NDK=~/ohos-sdk/linux/native
 fi
 
-export PATH=${NDK}/linux/native/build-tools/cmake/bin:$PATH
+export PATH=${NDK}/build-tools/cmake/bin:$PATH
 
 function build() {
     ABI=$1
     BUILD_PATH=build.OHOS.${ABI}
-    cmake -H. -DOHOS_STL=c++_shared -B${BUILD_PATH} -DOHOS_ARCH=${ABI} -DOHOS_PLATFORM=OHOS -DCMAKE_TOOLCHAIN_FILE=${NDK}/linux/native/build/cmake/ohos.toolchain.cmake -DQJS_NS=${QJSNS} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+    cmake -H. -DOHOS_STL=c++_shared -B${BUILD_PATH} -DOHOS_ARCH=${ABI} -DOHOS_PLATFORM=OHOS -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/ohos.toolchain.cmake -DQJS_NS=${QJSNS} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
     cmake --build ${BUILD_PATH} --config Release
-    #mkdir -p ./qjs/quickjs/Lib/OHOS/${ABI}/
-    #cp ${BUILD_PATH}/libquickjs.a ./qjs/quickjs/Lib/OHOS/${ABI}/libquickjs.a
+    mkdir -p ./qjs/quickjs/Lib/OHOS/${ABI}/
+    cp ${BUILD_PATH}/libquickjs.a ./qjs/quickjs/Lib/OHOS/${ABI}/libquickjs.a
 }
 
 build armeabi-v7a
