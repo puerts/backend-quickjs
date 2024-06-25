bce5b860588db213e8ae5e9304294516f565f575
johnche <johnche@tencent.com>
2024-06-25 19:23:52 +0800

    试试编译bc

---
+++ make_wasm_bc.sh
@@ -0,0 +1,20 @@
+if [ -n "$1" ]; then
+    export QJSNS=1
+else
+    export QJSNS=0
+fi
+
+git clone https://github.com/emscripten-core/emsdk.git
+cd emsdk
+git pull
+./emsdk install 3.1.8
+./emsdk activate 3.1.8
+source ./emsdk_env.sh
+cd ..
+
+mkdir -p build_wasm && cd build_wasm
+emcmake cmake -DQJS_NS=${QJSNS} -DBIYE_CODE=1 -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ../
+emmake make
+cd ..
+mkdir -p ./qjs/quickjs/Lib/wasm/bc
+cp build_wasm/libquickjs.bc ./qjs/quickjs/Lib/wasm/bc/
